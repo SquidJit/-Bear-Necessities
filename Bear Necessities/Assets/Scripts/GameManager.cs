1b960b854a7c10614417f0d9f4f9db157aaf6dcc
will-wrigh <43973227+will-wrigh@users.noreply.github.com>
2022-12-08 20:28:24 -0500

    attempt at ui

--- Bear Necessities/Assets/Scripts/GameManager.cs
+++ Bear Necessities/Assets/Scripts/GameManager.cs
@@ -22,4 +22,6 @@ public class GameManager : MonoBehaviour
             gameManager = this;
         }
     }
+
+
 }
--- Bear Necessities/Assets/Scripts/UI.cs
+++ Bear Necessities/Assets/Scripts/UI.cs
@@ -5,16 +5,22 @@ using TMPro;
 
 public class UI : MonoBehaviour
 {
+    private int health;
+    private int maxhealth;
+    public TextMeshProUGUI healthNumber;
+    public GameManager healthinstance;
 
 
     void Start()
     {
-
+        //health = GameObject.Find("Bear").GetComponent<PlayerBehavior>().
+        //health = healthinstance._playerHealth;
+        //maxhealth = GameManager.gameManager._playerHealth.MaxHealth;
     }
 
 
     void Update()
     {
-
+        healthNumber.SetText(health + "/" + maxhealth);
     }
 }
