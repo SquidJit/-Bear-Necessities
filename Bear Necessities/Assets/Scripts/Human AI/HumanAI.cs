59a9fa1c9e6b135213d62627760866fd7f159e5b
SquidJit <evanschmitz11@gmail.com>
2022-12-08 19:56:21 -0500

    Revert "Revert "Merge branch 'main' of https://github.com/SquidJit/-Bear-Necessities""
    This reverts commit 80bc4eed9b0968ed764c163c650d8c44391a125d.

---
+++ Bear Necessities/Assets/Scripts/Human AI/HumanAI.cs
@@ -0,0 +1,161 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class HumanAI : MonoBehaviour
+{ 
+
+      public NavMeshAgent navMeshAgent;
+      public float startWaitTime = 4;
+      public float timeToRotate = 2;
+      public float speedWalk = 6;
+      public float speedRun = 9;
+
+      public float viewRadius = 15;
+     public float viewAngle = 90;
+    public LayerMask playerMask;
+    public LayerMask obstacleMask;
+    public float meshResolution = 1f;
+    public int edgeIterations = 4;
+    public float edgeDistance = 0.5f;
+
+    public Transform[] waypoints;
+    int m_CurrentWaypointIndex;
+
+    Vector3 playerLastPosition = Vector3.zero;
+    Vector3 m_PlayerPosition;
+
+    float m_WaitTime;
+    float m_TimeToRotate;
+    bool m_PlayerInRange;
+    bool m_PlayerNear;
+    bool m_IsPatrol;
+    bool m_CaughtPlayer;
+
+    Animator animator;
+
+// Start is called before the first frame update
+    void Start()
+    {
+     m_PlayerPosition = Vector3.zero;
+     m_IsPatrol = true;
+     m_CaughtPlayer = false;
+     m_PlayerInRange = false;
+        m_WaitTime = startWaitTime;
+        m_TimeToRotate = timeToRotate;
+
+      m_CurrentWaypointIndex = 0;
+      navMeshAgent = GetComponent<NavMeshAgent>();
+
+      navMeshAgent.isStopped = false;
+      navMeshAgent.speed = speedWalk;
+     navMeshAgent.SetDestination(waypoints[m_CurrentWaypointIndex].position);
+
+        animator = GetComponent<Animator>();
+
+
+}
+
+// Update is called once per frame
+    void Update()
+    {
+     EnviromentView();
+     Patroling();
+
+        animator.SetFloat("Speed", navMeshAgent.velocity.magnitude);
+    }
+
+
+    private void Patroling()
+    {
+        if (m_PlayerNear)
+        {
+         if (m_TimeToRotate <= 0)
+         {
+             Move(speedWalk);
+            }
+         else
+            {
+               Stop();
+                m_TimeToRotate -= Time.deltaTime;
+          }
+     }
+      else
+     {
+          m_PlayerNear = false;
+          playerLastPosition = Vector3.zero;
+         navMeshAgent.SetDestination(waypoints[m_CurrentWaypointIndex].position);
+          if (navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance)
+          {
+               if (m_WaitTime <= 0)
+               {
+                  NextPoint();
+                    Move(speedWalk);
+                    m_WaitTime = startWaitTime;
+              }
+              else
+              {
+                Stop();
+                m_WaitTime -= Time.deltaTime;
+            }
+        }
+    }
+}
+
+    void Move(float speed)
+    {
+        navMeshAgent.isStopped = false;
+        navMeshAgent.speed = speed;
+    }
+
+    void Stop()
+    {
+     navMeshAgent.isStopped = true;
+     navMeshAgent.speed = 0;
+    }
+
+    public void NextPoint()
+    {
+     m_CurrentWaypointIndex = (m_CurrentWaypointIndex + 1) % waypoints.Length;
+      navMeshAgent.SetDestination(waypoints[m_CurrentWaypointIndex].position);
+    }
+
+
+
+
+
+    void EnviromentView()
+    {
+     Collider[] playerInRange = Physics.OverlapSphere(transform.position, viewRadius, playerMask);
+
+        for (int i = 0; i < playerInRange.Length; i++)
+        {
+          Transform player = playerInRange[i].transform;
+          Vector3 dirToPlayer = (player.position - transform.position).normalized;
+         if (Vector3.Angle(transform.forward, dirToPlayer) < viewAngle / 2)
+          {
+               float dstToPlayer = Vector3.Distance(transform.position, player.position);
+              if (!Physics.Raycast(transform.position, dirToPlayer, dstToPlayer, obstacleMask))
+             {
+                   m_PlayerInRange = true;
+                 m_IsPatrol = false;
+             }
+             else
+                {
+                  m_PlayerInRange = false;
+             }
+
+          }
+        if (Vector3.Distance(transform.position, player.position) > viewRadius)
+        {
+            m_PlayerInRange = false;
+        }
+        if (m_PlayerInRange)
+        {
+            m_PlayerPosition = player.transform.position;
+        }
+
+    }
+}
+}
